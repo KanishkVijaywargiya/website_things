e7ea0632c77aceab6788f8c4f7ec149bc93e6712
Kanishk <kanishkvijaywargiya99@gmail.com>
2020-06-23 16:11:49 +0530

    first commit

---
+++ true-parallax/trueParallax/js/func.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    lax.setup();
+    const updateLax = () => {
+        lax.update(window.scrollY);
+        window.requestAnimationFrame(updateLax);
+    }
+    window.requestAnimationFrame(updateLax);
+    window.addEventListener("resize", function () {
+        lax.updateElements();
+    });
+});
